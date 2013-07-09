40c3fd4ae2584e43335370291b6e3e1389cf81cc
atans <atans.chiu@gmail.com>
2013-07-09 14:33:55 +0800

    Fix Typos

--- Module.php
+++ Module.php
@@ -8,7 +8,7 @@ class Module
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
             ),
         );
@@ -33,5 +33,4 @@ class Module
             ),
         );
     }
-
 }
